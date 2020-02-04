92fa68703e0a190b2b324c6bddb02a1e636af529
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-04 15:14:17 +0100

    Progress up to defining ATM funds

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,35 @@
 class Atm
     attr_accessor :funds
+    require 'date' 
     def initialize
         @funds = 1000
+      end
+    
+    def withdraw(amount, account)
+        case
+        when insufficient_funds_in_account?(amount, account)
+            { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
+        else
+            perform_transaction(amount, account)
+        end
+    end
+
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+        amount > account.balance
+    end
+
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+    end
+
+    def perform_transaction(amount, account)
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
     end
 end
 
