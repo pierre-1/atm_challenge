ef68b49d0651c2a71bce2d35e2ae7e770d245926
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-05 09:37:17 +0100

    Added test for disabled account

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,6 @@
 require './lib/atm.rb'
 describe Atm do
-    let(:account) { instance_double('Account', pin_code: '1234') }
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/20', account_status: :active) }
     
     before do
     # Before each test we need to add an attribute of `balance`
@@ -34,5 +34,16 @@ describe Atm do
     it 'reject withdraw if the pin is wrong' do
         expected_output = { status: false, message: 'wrong pin', date: Date.today }
         expect(subject.withdraw(50, 9999, account)).to eq expected_output
-      end
+    end
+    it 'reject withdraw if the card is expired' do
+        allow(account).to receive(:exp_date).and_return('12/15')
+        expected_output = { status: false, message: 'card expired', date: Date.today }
+        expect(subject.withdraw(6, '1234', account)).to eq expected_output
+    end
+    it 'reject withdraw if account is disabled' do
+        allow(account).to receive(:account_status).and_return(:disabled)
+        expected_output = { status: false, message: 'account disabled', date: Date.today }
+        expect(subject.withdraw(100, '1234', account)).to eq expected_output
+    end
+
 end
